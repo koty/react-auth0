6fe5d8f0c81f89668419558af80f143c5639af72
koty <kouichi.nishizawa+github@gmail.com>
2020-08-03 21:39:06 +0900

    auth0を導入

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,9 +3,11 @@ import '../App.css'
 import { IStoreProvider, ITodoState } from '../interface'
 import { Store } from './Store'
 import { Button } from 'antd-mobile'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const App: React.FC = (): JSX.Element => { 
-  const { state, dispatch }: IStoreProvider = React.useContext(Store);
+  const { state, dispatch }: IStoreProvider = React.useContext(Store)
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0()
 
   const [value, setValue] = React.useState<string>("");
 
@@ -20,8 +22,24 @@ const App: React.FC = (): JSX.Element => {
     });
     setValue("");
   };
+  const handleLogin = () => {
+    loginWithRedirect();
+  }
   return (
     <div className="App">
+      <div>
+        {isAuthenticated ? (
+          <div>
+            <span>{user.nickname}</span>
+            <span>{user.email}</span>
+            <img src={user.picture} alt="user" />
+          </div>
+        ) : (
+          <div>
+            <Button onClick={handleLogin}>Login</Button>
+          </div>
+        ) }
+      </div>
       <div>
         <input value={value} onChange={e => setValue(e.target.value)} />
         <Button type="primary" onClick={handleSubmit} >Add Todo</Button>
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,18 @@ import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import { StoreProvider } from "./components/Store"
 import 'antd-mobile/dist/antd-mobile.css'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 ReactDOM.render(
-  <StoreProvider>
-    <App />
-  </StoreProvider>,
-  document.getElementById('root')
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
+    redirectUri={window.location.origin}>
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+  </Auth0Provider>,
+  document.querySelector('#root')
 );
 
 // If you want your app to work offline and load faster, you can change
